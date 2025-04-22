7c77146d3bdcf9ac7517fdf99b2101d1d0b0153e
GuilhermeGGF <guilhermegtfaria@gmail.com>
2025-04-22 15:25:15 -0300

    Update

---
+++ Programação Fullstack/Aulas/Aula_10/server.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');;
+
+var server = http.createServer(app);
+
+server.listen(80);
+
+console.log('Servidor rodando na porta 80');
+
+
+// Métodos + Actions
+
+app.get("/inicio", function(requisicao, resposta) {
+    resposta.redirect("Aula_1/index.html")
+});
+
+app.post("/inicio", function(requisicao, resposta) {
+    resposta.redirect("Aula_1/index.html")
+});
+
+app.post("/cadastrar", function(requisicao, resposta) {
+    let user = requisicao.body.user;
+    let nome = requisicao.body.nome;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nasc;
+    resposta.render("resposta", {user, nome, senha, nasc})
+});
