d4a77a20b263d2619aeda09d238b353281b0754a
GUILHERME GREGORUTTI FARIA <unieguifaria>
2025-04-29 16:37:24 -0300

    Update

---
+++ Programação Fullstack/Aulas/Aula_11/server.js
@@ -0,0 +1,76 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://teste:teste@cluster0.izygvwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+var dbo = client.db("teste");
+var users = dbo.collection("users");
+
+var app = express();
+
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');;
+
+var server = http.createServer(app);
+
+server.listen(80);
+
+console.log('Servidor rodando na porta 80');
+
+// Métodos + Actions
+
+app.get("/inicio", function(requisicao, resposta) {
+    resposta.redirect("Aula_1/index.html")
+});
+
+app.post("/inicio", function(requisicao, resposta) {
+    resposta.redirect("Aula_1/index.html")
+});
+
+app.post("/cadastrar", function(requisicao, resposta) {
+    let user = requisicao.body.user;
+    let nome = requisicao.body.nome;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nasc;
+
+    var data = { db_nome: nome, db_login: user, db_senha: senha, db_nasc: nasc };
+
+    users.insertOne(data, function (err) {
+        console.log(err)
+        if (err) {
+            resposta.render('resposta', {resposta: "Erro ao cadastrar usuário!"})
+        }else {
+            resposta.render('resposta', {resposta: "Usuário cadastrado com sucesso!"})
+        };
+      });
+    });
+
+app.post("/logar", function(requisicao, resposta) {
+    let nome = requisicao.body.user;
+    let senha = requisicao.body.senha;
+
+    var data = {db_login: nome, db_senha: senha };
+
+    users.find(data).toArray(function(err, items) {
+      console.log(items);
+      if (items.length == 0) {
+        resposta.render('resposta', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resposta.render('resposta', {resposta: "Erro ao logar usuário!"})
+      }else {
+        resposta.render('resposta', {resposta: "Usuário logado com sucesso!"})        
+      };
+    });
+  });
+
+
+app.get("/for_ejs", function(requisicao, resposta) {
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for", {valor});
+})
