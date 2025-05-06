bf647ead7e322590ca50ade2ee5592c12ba72d72
GUILHERME GREGORUTTI FARIA <unieguifaria>
2025-05-06 17:00:33 -0300

    Update

--- Programação Fullstack/Aulas/Aula_11/server.js
+++ Programação Fullstack/Aulas/Aula_11/server.js
@@ -68,9 +68,59 @@ app.post("/logar", function(requisicao, resposta) {
       };
     });
   });
+  
+  app.get("/listar", function(requisicao, resposta) {
+    users.find().toArray(function(err, usuarios) {
+      if (err) {
+        resposta.status(500).json({ erro: "Erro ao buscar usuários!" });
+      } else {
+        //resposta.json(usuarios);
+        let nome = usuarios.nome;
+        let nasc = usuarios.nasc;
+        let user = usuarios.user;
+        let senha = usuarios.senha;
+        resposta.render('listar', {listar: nome, nasc, user, senha})
+      }
+    });
+  });
+
+  app.post("/atualizar_senha", function(req, resposta) {
+    var data = { db_login: req.body.user, db_senha: req.body.senha };
+    var newData = { $set: {db_senha: req.body.novasenha} };
+
+    users.updateOne(data, newData, function (err, result) {
+      console.log(result);
+      if (result.modifiedCount == 0) {
+        resposta.render('resposta', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resposta.render('resposta', {resposta: "Erro ao atualizar usuário!"})
+      }else {
+        resposta.render('resposta', {resposta: "Usuário atualizado com sucesso!"})        
+      };
+    });
+  });
 
 
-app.get("/for_ejs", function(requisicao, resposta) {
-    let valor = requisicao.query.valor;
-    resposta.render("exemplo_for", {valor});
-})
+     
+  app.post("/remover_usuario", function(req, resposta) {
+    var data = { db_login: req.body.user, db_senha: req.body.senha };
+   
+    users.deleteOne(data, function (err, result) {
+      console.log(result);
+      if (result.deletedCount == 0) {
+        resposta.render('resposta', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resposta.render('resposta', {resposta: "Erro ao remover usuário!"})
+      }else {
+        resposta.render('resposta', {resposta: "Usuário removido com sucesso!"})        
+      };
+    });
+  });
+
+
+
+
+  app.get("/for_ejs", function(requisicao, resposta) {
+      let valor = requisicao.query.valor;
+      resposta.render("exemplo_for", {valor});
+  })
